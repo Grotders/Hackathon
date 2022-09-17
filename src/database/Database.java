e81b3991aea619404ae62547ae88ce25ae1fd50e
serhatoluc <serhatoluc@hotmail.com>
2022-09-17 19:39:43 +0300

    version 2

--- src/database/Database.java
+++ src/database/Database.java
@@ -20,10 +20,10 @@ public class Database {
         FmssGame.games.add(new Game(0, "Fallout", 8.5, 60, List.of(GameGenre.ACTION)));
         FmssGame.games.add(new Game(1, "FallGuys", 8.5, 60, List.of(GameGenre.ACTION)));
         FmssGame.games.add(new Game(2, "Battlefield", 8.5, 60, List.of(GameGenre.ACTION)));
-        FmssGame.games.add(new Game(3, "CounterStrike", 8.5, 60, List.of(GameGenre.ACTION)));
+        FmssGame.games.add(new Game(3, "CounterStrike", 8.5, 0, List.of(GameGenre.ACTION)));
 
         users.add(new User(0, "Oguzcan", 155, List.of(gameById(1), gameById(2))));
-        users.add(new User(1, "Emrullah", 5, List.of(gameById(3), gameById(1))));
+        users.add(new User(1, "Emrullah", 5, new ArrayList<>()));
         users.add(new User(2, "Serhat", 15, List.of(gameById(2), gameById(0))));
 
     }
